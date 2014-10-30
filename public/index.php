0705eb06e44d8684fdbf23e6243086d6c096d0e8
Jeroen Bleijenberg <jeroen@maxserv.nl>
2014-10-30 19:54:04 +0100

    Moved application root to public folder to keep application, lib and config secure from the outside world

--- public/index.php
+++ public/index.php
@@ -24,11 +24,11 @@
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-define('BP', dirname(realpath(__FILE__)));
+define('BP', dirname(dirname(realpath(__FILE__))));
 define('DS', DIRECTORY_SEPARATOR);
 
-require_once 'lib/Functions.php';
-require_once 'lib/Autoloader.php';
+require_once '../lib/Functions.php';
+require_once '../lib/Autoloader.php';
 
 $di = new \Jcode\DependencyContainer;
 
